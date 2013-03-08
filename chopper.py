42cae7bc23aa3c20b1553273c86395ec17755412
Sam Phippen <samphippen@googlemail.com>
2013-03-08 19:22:04 +0000

    Add the final chopper implementation
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- chopper.py
+++ chopper.py
@@ -30,3 +30,26 @@ def chop2(item, values, min = 0, max = None):
         return chop2(item, values, mid+1, max)
     else:
         return mid
+
+def list_midpoint(values):
+    return midpoint(0,len(values))
+
+def left_subtree(values):
+    return values[:list_midpoint(values)]
+
+def right_subtree(values):
+    return values[list_midpoint(values)+1:]
+
+def chop3(item, values):
+    if len(values) == 0:
+        return -1
+
+    mid = list_midpoint(values)
+
+    if values[mid] > item:
+        return chop3(item, left_subtree(values))
+    elif values[mid] < item:
+        chop_result = chop3(item, right_subtree(values))
+        return mid + 1 + chop_result if chop_result != -1 else -1
+    else:
+        return mid
